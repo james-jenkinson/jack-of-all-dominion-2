490cae4a2b670dbb054e45db895f1620d1585bd3
James Jenkinson <james@ctrl.alt.coop>
2024-05-26 10:34:23 +0200

    Add translation provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { type JSX } from 'solid-js'
 import './App.css'
+import { useLanguage } from './contexts/languageContext'
 
 function App (): JSX.Element {
+  const { t } = useLanguage()
+
   return (
     <>
       <h1>Hello</h1>
+      <p>
+        {t('Test not working :(')}
+      </p>
     </>
   )
 }
